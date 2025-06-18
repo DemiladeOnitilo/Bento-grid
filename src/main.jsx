8b4d406fa0d7d9d2bc20dc6d6fffef9695bef603
DemiladeOnitilo <137829149+DemiladeOnitilo@users.noreply.github.com>
2025-06-18 13:29:10 +0100

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { Provider } from 'react-redux'
+import { store, persistor } from './store/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </PersistGate>
+  </Provider>
 )
